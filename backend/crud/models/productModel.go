b74ca2c3538b01ff01bc9154a0516419fd7e49b0
Tukan Yazılım <86947271+SZaferUlgur@users.noreply.github.com>
2024-11-20 20:07:18 +0300

    Update productModel.go

--- backend/crud/models/productModel.go
+++ backend/crud/models/productModel.go
@@ -1,1 +1,14 @@
+package models
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+type Product struct {
+	ID       int             `json:"id"`
+	UrunAdi  string          `json:"urunAdi"`
+	Fiyat    decimal.Decimal `json:"fiyat"`
+	Miktar   decimal.Decimal `json:"miktar"`
+	ImageUrl string          `json:"imageUrl"`
+}
 
